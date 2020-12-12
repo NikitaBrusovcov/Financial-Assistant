8f1fe4a4fcd34b639137a32048c95beb207ef4b0
Nik Krein <nikkrein1@gmail.com>
2020-12-12 19:56:26 +0300

    Edt filters

--- src/main/java/bsu/tp/financial/filter/AdminSessionFilter.java
+++ src/main/java/bsu/tp/financial/filter/AdminSessionFilter.java
@@ -7,6 +7,17 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class AdminSessionFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) servletRequest;
--- src/main/java/bsu/tp/financial/filter/LoginFilter.java
+++ src/main/java/bsu/tp/financial/filter/LoginFilter.java
@@ -7,6 +7,17 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class LoginFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) servletRequest;
