948ba0d4fbf47116f957a540cff195c2951368d3
Alexander <lebedevskiy_s@rambler.ru>
2020-12-12 16:08:19 +0300

    Add webapp-runner

--- src/main/java/bsu/tp/financial/filter/EncodingFilter.java
+++ src/main/java/bsu/tp/financial/filter/EncodingFilter.java
@@ -8,6 +8,16 @@ import java.nio.charset.StandardCharsets;
 @WebFilter("/*")
 public class EncodingFilter implements Filter {
     @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         servletRequest.setCharacterEncoding(StandardCharsets.UTF_8.name());
         servletResponse.setCharacterEncoding(StandardCharsets.UTF_8.name());
